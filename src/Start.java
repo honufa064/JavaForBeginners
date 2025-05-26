3582ecd30bc57ff389d76dd7809e4f8a757db2b9
FORHAD <forhadulislam@gmail.com>
2025-05-26 10:53:50 +0900

    Does sensei Teach java

--- src/Start.java
+++ src/Start.java
@@ -4,8 +4,8 @@ public class Start {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("what is programing language");
-		MyFrame frame1= new RectText();
-
+		MyFrame frame1= new OvalTest();
+     
 	}
 
 }
