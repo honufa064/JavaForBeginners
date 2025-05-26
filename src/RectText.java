57bad47b2b0e408e85fd80f1e03b2f459cbf6100
FORHAD <forhadulislam@gmail.com>
2025-05-26 10:52:15 +0900

    what is java

---
+++ src/RectText.java
@@ -0,0 +1,26 @@
+
+public class RectText extends MyFrame  {
+	public void run() {
+		setColor(255,255,0);
+		fillRect(50,50,100,100);
+		
+		setColor(255,0,255);
+		fillRect(150,50,100,100);
+		
+		setColor(0,255,255);
+		fillRect(50,160,100,100);
+		
+		setColor(0,0,0);
+		fillRect(160,160,100,100);
+		
+		setColor(128,128,128);
+		fillRect(270,160,100,100);
+		
+		setColor(255,255,255);
+		fillRect(295,185,50,50);
+		
+		
+		
+	}
+
+}
--- src/Start.java
+++ src/Start.java
@@ -3,6 +3,8 @@ public class Start {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("what is programing language");
+		MyFrame frame1= new RectText();
 
 	}
 
