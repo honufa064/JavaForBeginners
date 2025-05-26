98b3d7d635bc8cf64fb1ec420152ec17fb79fe42
FORHAD <forhadulislam@gmail.com>
2025-05-26 11:41:14 +0900

    fugf

---
+++ src/RectANDoval.java
@@ -0,0 +1,14 @@
+
+
+public class RectANDoval extends MyFrame {
+	public void run () {
+		
+		fillRect(25,130,100,15);
+		fillRect(50,110,50,30);
+		fillOval(85,140,20,20);
+		fillOval(45,140,20,20);
+		
+		
+	}
+
+}
