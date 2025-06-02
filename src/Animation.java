b837822674649e6b17ec170eff81a24db8066ebc
FORHAD <forhadulislam@gmail.com>
2025-06-02 11:47:34 +0900

    animation.java

--- src/Animation.java
+++ src/Animation.java
@@ -22,7 +22,66 @@ public class Animation extends MyFrame {
 			fillRect(x,y,10,100);
 			x+=5;
 			sleep (0.1);
-			
+			  //5-2
+				   while(y <= 200) {
+				    //①画面を消す
+			 	clear();
+					//②四角形を表示する
+			   setColor(0,128,0);
+				   fillRect(x,y,10,100);
+				   x += 5;
+				   y += 5;
+				   sleep(0.1);
+				  }
+			     while(x >= 30) {
+				//①画面を消す
+				    clear();
+					//②四角形を表示する
+				 	setColor(0,128,0);
+			 	fillRect(x,y,10,100);
+				 	x -= 5;
+				 	sleep(0.1);
+				 }
+			      while(y >= 100) {
+					//①画面を消す
+				 	clear();
+					//②四角形を表示する
+				 	setColor(0,128,0);
+				 	fillRect(x,y,10,100);
+				 	x += 5;
+				    y -= 5;
+			 	sleep(0.1);
+				 }
+			    while(x >= 30) {
+					//①画面を消す
+				 	clear();
+					//②四角形を表示する
+				 	setColor(0,128,0);
+				    fillRect(x,y,10,100);
+				 	x -= 5;
+				 	sleep(0.1);
+			     }
+				
+			 	
+				for (int i = 0; i < 3; i++) {
+			   // → 右へ進む
+			    for (; x <= 300; x += 5) {
+		        clear();
+		       setColor(0, 128, 0);
+		        fillRect(x, y, 10, 100);
+			        sleep(0.1);
+			    }
+	 
+			    //← 左へ戻る
+		    for (; x >= 30; x -= 5) {
+		        clear();
+		        setColor(0, 128, 0);
+		        fillRect(x, y, 10, 100);
+		        sleep(0.1);
+			    
+			}
+	 
+		   }
 		
 		
 		}
