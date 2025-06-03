85764b83431b1f0f268bc2956ba3fac0db4aaa5e
FORHAD <forhadulislam@gmail.com>
2025-06-03 10:00:36 +0900

    Strat.java

--- src/Start.java
+++ src/Start.java
@@ -4,7 +4,7 @@ public class Start {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("what is programing language");
-		MyFrame f= new DisplayFaceObject();
+		MyFrame f= new DisplayFaceAndTree();
      
 	}
 
